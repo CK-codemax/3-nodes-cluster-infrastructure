ceb95cd4b50be1e88e2d167049019cb4d618943d
Ochuko Whoro <ckcodemax@gmail.com>
2025-10-04 12:58:27 +0100

    removed outputs not needed

--- output.tf
+++ output.tf
@@ -7,54 +7,14 @@ output "ssh_commands" {
   }
 }
 
-output "master_instance_ids" {
-  description = "IDs of the master instances"
-  value       = aws_instance.masters[*].id
-}
-
-output "master_private_ips" {
-  description = "Private IP addresses of the master instances"
-  value       = aws_instance.masters[*].private_ip
-}
-
 output "master_public_ips" {
   description = "Public IP addresses of the master instances"
   value       = aws_instance.masters[*].public_ip
 }
 
-output "worker_instance_ids" {
-  description = "IDs of the worker instances"
-  value       = aws_instance.workers[*].id
-}
-
-output "worker_private_ips" {
-  description = "Private IP addresses of the worker instances"
-  value       = aws_instance.workers[*].private_ip
-}
-
 output "worker_public_ips" {
   description = "Public IP addresses of the worker instances"
   value       = aws_instance.workers[*].public_ip
-}
-
-
-output "key_pair_name" {
-  description = "Name of the key pair"
-  value       = aws_key_pair.main.key_name
-}
-
-output "ami_id" {
-  description = "ID of the AMI used"
-  value       = data.aws_ami.ubuntu.id
-}
-
-output "cluster_info" {
-  description = "Cluster information"
-  value = {
-    cluster_name      = var.cluster_name
-    kubernetes_version = var.kubernetes_version
-    region           = var.aws_region
-  }
 }
 
 
